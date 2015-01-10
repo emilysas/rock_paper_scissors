36b7667035f26a03e7282019a5d659ea472849e3
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-10 10:59:06 +0000

    second cucumber scenario written

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class Rock_Paper_Scissors < Sinatra::Base
   get '/' do
-    'Hello Rock_Paper_Scissors!'
+    erb :get_name
   end
 
   # start the server if ruby file executed directly
